a11fb4176c835ef9bbfb57267b0dc43ee2fa7bdb
seo ho jin <aku7777@naver.com>
2020-07-19 18:24:34 +0900

    update

--- add-nbo/add-nbo.cpp
+++ add-nbo/add-nbo.cpp
@@ -3,8 +3,8 @@
 
 
 uint32_t my_ntohl(uint32_t n) {
-	printf("0x%x\n",n);
-    return (n & 0x000000FF) << 24 | (n & 0x0000FF00) << 8 | (n & 0x00FF0000) >> 8 | (n & 0xFF000000) >> 24;
+	//printf("0x%x\n",n);
+	return (n & 0x000000FF) << 24 | (n & 0x0000FF00) << 8 | (n & 0x00FF0000) >> 8 | (n & 0xFF000000) >> 24;
 }
 
 
@@ -12,7 +12,7 @@ int main(int argc, char * argv[]){
 	
 	FILE *fp, *fp2;
 	uint8_t network_buf[4];
-    uint8_t network_buf2[4];
+	uint8_t network_buf2[4];
 
 	fp = fopen(argv[1],"rb");
 	fp2 = fopen(argv[2],"rb");
